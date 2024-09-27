cb358ec337d3968ae1fab97d9393221ff6c0c141
qwerty1121gif <Vladimirstalin@tutamail.com>
2024-09-27 11:53:01 +0300

    123

---
+++ zad1.py
@@ -0,0 +1,8 @@
+input_data = open('input.txt', 'r')
+data = input_data.read()
+data = data.split()
+c = str(int(data(0)) + int(data(1)))
+output_data = open('output.txt', 'w')
+output_data.write(c)
+input_data.close()
+output_data.close()
